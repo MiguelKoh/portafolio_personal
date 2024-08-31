a79b2a77eeda74575200d46226d90e232b8d7e1b
Miguel <miguelkohavila@gmail.com>
2024-08-31 00:41:19 -0600

    se quito el console.log de jobs

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -7,7 +7,7 @@ function Experiencia() {
   const [t]= useTranslation("global");
   const jobs = t('jobs', { returnObjects: true });
 
-   console.log(jobs)
+   
   return (
     <section className="experience_section nav_section" id="experience">
       <div className="container" data-aos="fade-up" data-aos-delay="100" >
